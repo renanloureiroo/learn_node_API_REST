ee19519b68c78dd7e592e8b1e683323292e67aba
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-09-30 23:36:51 -0300

    CRUD user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -28,6 +28,40 @@ class UserController {
       return res.json(null);
     }
   }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      user.update({
+        nome: req.body.nome,
+        password: req.body.password,
+      });
+
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      user.destroy();
+
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
 }
 
 export default new UserController();
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,8 +3,11 @@ import UserController from '../controllers/UserController';
 
 const router = new Router();
 
-router.get('/', UserController.index);
 router.post('/', UserController.create);
+router.put('/:id', UserController.update);
+router.get('/', UserController.index);
+router.get('/:id', UserController.show);
+router.delete('/:id', UserController.delete);
 
 export default router;
 
