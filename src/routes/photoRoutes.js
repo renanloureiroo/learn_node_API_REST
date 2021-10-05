d55e7698bae17e7dc489f2155192bcb9d66ed622
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-10-05 20:31:57 -0300

    upload photos login required

--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import loginRequired from '../middlewares/loginRequired';
 
 import PhotoController from '../controllers/PhotoController';
 
 const router = new Router();
 
-router.post('/', PhotoController.create);
+router.post('/', loginRequired, PhotoController.create);
 
 export default router;
